3ac40f3ab32b5f570733416491c448fa92dc54c5
sphere <sphere@dersphere.de>
2011-05-01 01:40:57 +0200

    added using of label_solution and image_solution

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -71,6 +71,10 @@ class GUI(xbmcgui.WindowXMLDialog):
         self.button_random.setLabel(getString(self.SID_RANDOM))
         self.button_exit.setLabel(getString(self.SID_EXIT))
 
+        # set control visibility
+        self.label_solution.setVisible(False)  # fixme, could be earlier but conflicts with startApi and getControl
+        self.image_solution.setVisible(False)  # fixme
+
         # start the api
         self.login()
         self.getRandomShot()
@@ -140,10 +144,14 @@ class GUI(xbmcgui.WindowXMLDialog):
 
     def answerRight(self, title_year):
         message = getString(self.SID_ANSWER_RIGHT)
+        self.label_solution.setVisible(True)
+        self.image_solution.setVisible(True)
+        self.label_solution.setLabel(title_year)
         self.getRandomShot()
         self.score += 1
         self.updateScore()
-        print title_year  # fixme: plz put me in any label
+        self.label_solution.setVisible(False)
+        self.image_solution.setVisible(False)
         dialog = xbmcgui.Dialog()
         dialog.ok('right', message)  # fixme
 
